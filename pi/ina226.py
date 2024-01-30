23778ba8b5ee2a1c07920b34232d141a1bc8183b
Joshua Vial <joshua@enspiral.com>
2024-01-31 00:03:32 +1300

    Add debug print statement for post data before sending the request.

--- pi/ina226.py
+++ pi/ina226.py
@@ -127,6 +127,7 @@ try:
                     'to': int(end_time * 1e6),
                     'energy': energy_consumed_J
                 }
+                print(post_data)
                 response = requests.post('http://192.168.1.99:3000/observation', json=post_data)
                 if response.status_code == 200:
                     print(f"Energy data posted successfully: {post_data}")
