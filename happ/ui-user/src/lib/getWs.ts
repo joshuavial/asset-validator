e153b36dcc7ba44e8d5f86a894a9e2e4e463756b
Joshua Vial <joshua@enspiral.com>
2024-02-04 19:04:37 +1300

    Refactored getWs.ts to use URL constructor for AdminWebsocket connection.

--- happ/ui-user/src/lib/getWs.ts
+++ happ/ui-user/src/lib/getWs.ts
@@ -3,12 +3,10 @@ import { AdminWebsocket, AppAgentWebsocket } from "@holochain/client";
 
 export async function getWsURL() {
   //copy the admin ws url from holochain playground
-  console.log(process)
-export async function getWsURL() {
   const ADMIN_WS_URL = `ws://127.0.0.1:${import.meta.env.VITE_WC_ADMIN_PORT}`;
-  const INSTALLED_APP_ID = "asset-validator";
+  //const INSTALLED_APP_ID = "asset-validator";
 
-  const adminWs = await AdminWebsocket.connect(ADMIN_WS_URL);
+  const adminWs = await AdminWebsocket.connect(new URL(ADMIN_WS_URL));
   const appInterfaces = await adminWs.listAppInterfaces();
   //paste this into your ui App.svelte
   return (`ws://127.0.0.1:${appInterfaces[0]}`);
