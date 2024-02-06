e198f6d21f595c4256258acc8809d04ded04d5ef
Joshua Vial <joshua@enspiral.com>
2024-02-06 14:09:42 +1300

    Add server.ts for user module.

---
+++ happ/server/user/server.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+import { CellType, GrantedFunctionsType, AdminWebsocket} from "@holochain/client";
+
+const app = express()
+app.use(cors({
+  origin: 'http://localhost:3000', // or the specific origin you want to allow
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
+
+const ADMIN_WS_PORT = process.env.WC_ADMIN_PORT
+const ADMIN_WS_URL = new URL(`ws://127.0.0.1:${ADMIN_WS_PORT}`);
+
+export async function getAgentWsURL() {
+  const adminWs = await AdminWebsocket.connect(ADMIN_WS_URL);
+  const appInterfaces = await adminWs.listAppInterfaces();
+  return (`ws://127.0.0.1:${appInterfaces[0]}`);
+}
+
+app.post('/grant', async (req, res) => {
+  const {signingKey, keyPair} = req.body
+  const cell_id = ''
+  const adminWs = await AdminWebsocket.connect(ADMIN_WS_URL);
+  const capSecret = await adminWs.grantSigningKey(
+    cell_id,
+    { [GrantedFunctionsType.All]: null },
+    signingKey
+  );
+  const signingCredentials = {
+    capSecret,
+    keyPair,
+    signingKey,
+  };
+  res.json({signingCredentials})
+})
+
+app.get('/agent_ws', async (_, res) => {
+  const agent_ws_url = await getAgentWsURL()
+  res.json({agent_ws_url})
+})
+
+
+export default app
