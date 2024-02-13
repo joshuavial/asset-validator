26d92548fd3dda40c7c7621cea32527e4b975eae
Joshua Vial <joshua@enspiral.com>
2024-02-13 15:30:12 +1300

    Add timestamp and header to ActiveGeneration component.

--- happ/ui-user/src/ActiveGeneration.svelte
+++ happ/ui-user/src/ActiveGeneration.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 import type { GenerationWithHash } from './asset_validator/validation_claims/types';
+import { formatDistanceToNow } from 'date-fns';
 
 export let activeGeneration: GenerationWithHash;
+let timestamp = new Date(activeGeneration.record.signed_action.hashed.content.timestamp);
+let timeAgo = formatDistanceToNow(timestamp, { addSuffix: true });
 
 </script>
+
+<h1>Your generation</h1>
+<p>Generated {timeAgo}</p>
+{activeGeneration.record}
