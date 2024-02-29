ec7ea31ece101f5fe6eb8fd41b5e2921f2980fbb
Joshua Vial <joshua@enspiral.com>
2024-02-29 09:06:08 -0700

    welcome text

--- happ/ui-user/src/Welcome.svelte
+++ happ/ui-user/src/Welcome.svelte
@@ -1,1 +1,3 @@
-Hi, overview of the application
+<p>
+Welcome, this is a demonstration of using Holochain as a validated data layer to integrate a large decentralised data store with onchain contracts.
+</p>
