42188d034792e592eb3b4228adb6f57d16b4e047
Joshua Vial <joshua@enspiral.com>
2024-02-29 01:37:43 -0700

    Add initialization for `wattbikeUrl` variable.

--- happ/ui-agent/src/asset_validator/validation_claims/GenerationDetail.svelte
+++ happ/ui-agent/src/asset_validator/validation_claims/GenerationDetail.svelte
@@ -44,6 +44,7 @@ let editing = false;
 let errorSnackbar: Snackbar;
 
 $: editing,  error, loading, record, generation, timeAgo, observations;
+let wattbikeUrl = '';
 let totalJoulesGeneratedFormatted: string;
 let totalJoulesGenerated: number;
 
