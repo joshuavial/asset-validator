69c0537394b8652bb800f2813a753e79db445a4f
Joshua Vial <joshua@enspiral.com>
2024-03-06 13:13:10 +1300

    aider: Add functionality to download all users in a CSV format.
    # Aider chat conversation:
    
    USER: add a download link which will download all the users in a csv
    
    ASSISTANT: ```diff
    --- happ/ui-agent/src/Users.svelte
    +++ happ/ui-agent/src/Users.svelte
    @@ ... @@
     <script lang="ts">
     import { onMount, getContext } from 'svelte';
     import { clientContext } from './contexts';
    +import { saveAs } from 'file-saver';
     import type { Link } from '@holochain/client';
     import { decode } from '@msgpack/msgpack';
     import type { EthUser } from './asset_validator/eth_user/types';
    
     let client = (getContext(clientContext) as any).getClient();
     let ethUsers: Map<string, EthUser> = new Map();
     let loading = true;
     let error: any = undefined;
    
    +function downloadCSV() {
    +  const csvContent = "data:text/csv;charset=utf-8,"
    +    + "Eth Address,Handle\n"
    +    + Array.from(ethUsers.values())
    +      .map(u => `${u.eth_address},${u.handle}`)
    +      .join("\n");
    +  const encodedUri = encodeURI(csvContent);
    +  saveAs(encodedUri, "eth_users.csv");
    +}
    +
     onMount(async () => {
       try {
         const links = await client.callZome({
    @@ ... @@
     </script>
    
     {#if loading}
     <div>Loading...</div>
     {:else if error}
     <div>Error: {error}</div>
     {:else}
    +<button on:click={downloadCSV}>Download CSV</button>
     <div>
       <h1>Eth Users</h1>
       {#each Array.from(ethUsers.values()) as user}
         <p>{user.eth_address} - {user.handle}</p>
       {/each}
     </div>
     {/if}
    ```
    
    This diff adds a `downloadCSV` function to the `Users.svelte` file, which when called, will generate a CSV string from the `ethUsers` map and trigger a download of the file named "eth_users.csv". It also adds a button to the UI that calls this function when clicked. The `file-saver` library is used to save the file, which needs to be installed if not already present in the project.

--- happ/ui-agent/src/Users.svelte
+++ happ/ui-agent/src/Users.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { onMount, getContext } from 'svelte';
 import { clientContext } from './contexts';
+import { saveAs } from 'file-saver';
 import type { Link } from '@holochain/client';
 import { decode } from '@msgpack/msgpack';
 import type { EthUser } from './asset_validator/eth_user/types';
@@ -9,6 +10,16 @@ let client = (getContext(clientContext) as any).getClient();
 let ethUsers: Map<string, EthUser> = new Map();
 let loading = true;
 let error: any = undefined;
+
+function downloadCSV() {
+  const csvContent = "data:text/csv;charset=utf-8," 
+    + "Eth Address,Handle\n"
+    + Array.from(ethUsers.values())
+      .map(u => `${u.eth_address},${u.handle}`)
+      .join("\n");
+  const encodedUri = encodeURI(csvContent);
+  saveAs(encodedUri, "eth_users.csv");
+}
 
 onMount(async () => {
   try {
@@ -45,6 +56,7 @@ onMount(async () => {
 {:else if error}
 <div>Error: {error}</div>
 {:else}
+<button on:click={downloadCSV}>Download CSV</button>
 <div>
   <h1>Eth Users</h1>
   {#each Array.from(ethUsers.values()) as user}
